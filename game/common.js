5433756fa7a480a1818d10bc248c1d6125101cac
Dzianis Pryshchep <dzianis_pryshchep@epam.com>
2018-12-14 23:03:11 +0300

    Basic logic added for bricks initialization

--- game/common.js
+++ game/common.js
@@ -24,6 +24,27 @@ paddleDX = 7;
 var rightPressed = false;
 var leftPressed = false;
 
+// block
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(var c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for(var t = 0; t < brickRowCount; t++) {
+    bricks[c][t] = {
+      x: 0,
+      y: 0,
+      status: 1
+    };
+  }
+}
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -61,6 +82,38 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+function drawBricks() {
+  for(var c = 0; c < brickColumnCount; c++) {
+    for(var t = 0; t < brickRowCount; t++) {
+      if(bricks[c][t].status == 1) {
+        var brickX = (c*(brickWidth + brickPadding)) + brickOffsetLeft;
+        var brickY = (t*(brickHeight+brickPadding)) + brickOffsetTop;
+        bricks[c][t].x = brickX;
+        bricks[c][t].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
+function collisionDetection() {
+  for(var c = 0; c < brickColumnCount; c++) {
+    for(var t = 0; t < brickRowCount; t++) {
+      var b = bricks[c][t];
+      if(b.status == 1) {
+        if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+          dy = -dy;
+          b.status = 0;
+        }
+      }
+    }
+  }
+}
+
 function increaseCount() {
   document.getElementById("count").innerHTML = ++count;
 }
@@ -74,7 +127,9 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
-  
+  drawBricks();
+  collisionDetection();
+
   //collision model
   if (y + dy > canvas.height - r ) {
     dy = -dy;
